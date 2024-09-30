308c79971fc51e4634b54cb98cc92e2a1ce6dc15
marianrr <marianseether@gmail.com>
2024-09-30 17:00:29 +0300

    factory functions

---
+++ 6/main.js
@@ -0,0 +1,12 @@
+const Factory = (name) => {
+    return {name: name,
+        coco: function() {
+            return 'dj guga la felipa...'
+        }
+    }
+}
+
+const two = Factory("Je suis rb...")
+console.log("Numele este ", two.name)
+console.log(typeof two.name)
+console.log(two.coco())
