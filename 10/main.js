ae384bb85a583c1ca518894ed0bc64a3e22d2d23
emi web developer <marianseether@gmail.com>
2024-10-01 11:50:33 +0300

    Create object with classes

---
+++ 10/main.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
+
+
+const person1 = new Person("marian", 40)
+console.log(person1.name, person1.age)
