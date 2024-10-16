e6c02bb19056945c73ad35046eba3be4432fcb0c
emi web developer <marianseether@gmail.com>
2024-10-16 23:57:15 +0300

    linked list 1

---
+++ 30/aa.js
@@ -0,0 +1,53 @@
+// Make the Node and List links
+
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+
+    }
+
+}
+
+
+class List {
+    constructor() {
+        this.head = null
+        this.tail = null
+
+    }
+
+    addToHead(data) {
+        const node = new Node(data)
+        if (!this.head) {
+            this.head = node
+            this.tail = node
+        }
+
+        else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+
+    }
+
+
+
+
+}
+
+
+const list1 = new List()
+list1.addToHead(1)
+list1.addToHead(2)
+list1.addToHead(3)
+list1.addToHead(4)
+list1.addToHead(5)
+list1.addToHead(6)
+list1.addToHead(7)
+list1.addToHead(8)
+list1.addToHead(9)
+
+console.log("This is the head: ", list1.head)
+console.log("This is the tail: ", list1.tail)
